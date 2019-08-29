8593892ba2d8bca55824c0a0f42612e3bfdd1653
Amanda Regan <regan008@gmail.com>
2019-08-29 15:05:21 -0500

    (1)don't call data inside `server.R` since its in `global.R`. (2) Fix slider to start at 65 and not 63.

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ server <- function(input, output) {
    #current.year.data <- data %>% filter(Year == input$map_year)
     
   output$spaces_map <- renderLeaflet({
-    data <- readRDS("NGSdata/data.rds")
+    
     city_filter <- strsplit(input$map.city, ", ")[[1]]
     
     data <- data %>% filter(Year == input$map.year)
--- ui.R
+++ ui.R
@@ -17,7 +17,7 @@ fluidRow(
   column(9,
          leafletOutput("spaces_map"),
           sliderInput("map.year", "Year",
-                       min = 1963, max = 1980,
+                       min = 1965, max = 1980,
                        value = 1900, sep = "", round = TRUE, step = 1, width = "100%")))
           
 )
