90321caa81d5943e1d4d6e18cde9fc567f8b51a6
Amanda Regan <regan008@gmail.com>
2019-08-29 15:01:53 -0500

    get city filter working in concert with year filter for the map.

--- server.R
+++ server.R
@@ -1,33 +1,28 @@
 library("shiny")
 library("leaflet")
 library("DT")
+library("dplyr")
 
-data <- read.csv(file = "NGSData/CompleteDataset.csv", stringsAsFactors = FALSE)
-#Define server logic required to draw a histogram ----
 
-server <- function(input, output, session) {
+
+server <- function(input, output) {
   
-    selected.data <- reactive({
       
-      city_filter <- strsplit(input$map_city, ", ")[[1]]
+    
+   #current.year.data <- data %>% filter(Year == input$map_year)
+    
+  output$spaces_map <- renderLeaflet({
+    data <- readRDS("NGSdata/data.rds")
+    city_filter <- strsplit(input$map.city, ", ")[[1]]
+    
+    data <- data %>% filter(Year == input$map.year)
+    
+    if (all(city_filter != "All cities")) {
+      data <- data %>% 
+        filter(city == city_filter[1], state == city_filter[2])
+    }
       
-      current.year.data <- data %>% filter(Year == input$map_year)
-      
-      
-      if (all(city_filter != "All cities")) {
-        current.year.data <- current.year.data %>%
-          filter(city == city_filter[1], state == city_filter[2])
-      }
-      current.year.data
-    })
-  
-  
-  
-  
-    output$spaces_map <- renderLeaflet({
-      
-     
-       leaflet(current.year.data) %>% addTiles() %>%
+      leaflet(data) %>% addTiles() %>%
         addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
       
     })
--- ui.R
+++ ui.R
@@ -10,13 +10,13 @@ ui <- fluidPage(
 titlePanel(HTML("Mapping the Gay Guides")),
 fluidRow(
   column(3, wellPanel(
-    selectInput("map_city", "Choose a city:", cities),
+    selectInput("map.city", "Choose a city:", cities),
     selectInput("map_amenity_feature", "Choose an establishment feature:", c("Show All", "(PE) - Pretty Elegant", "Miami", "Nashville"), selected = "Show All", multiple = TRUE, selectize = TRUE, width = NULL, size = NULL),
     includeHTML("authors.html")
   )),
   column(9,
          leafletOutput("spaces_map"),
-          sliderInput("map_year", "Year",
+          sliderInput("map.year", "Year",
                        min = 1963, max = 1980,
                        value = 1900, sep = "", round = TRUE, step = 1, width = "100%")))
           
