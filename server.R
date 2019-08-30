9b0ff78533c6a5ea61dce0c2b746bc65bd9037a5
Amanda Regan <regan008@gmail.com>
2019-08-30 14:18:51 -0500

    Add datatable to bottom of app. Set up to filter by city & year like map.

--- server.R
+++ server.R
@@ -26,5 +26,26 @@ server <- function(input, output) {
         addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
       
     })
+  
+  data.for.table <- reactive({
+   
+  })
+  
+  
+  output$spaces.table <- renderDataTable({
+    city_filter <- strsplit(input$map.city, ", ")[[1]]
+    
+    dft <- data %>%
+      filter(Year == input$map.year)
+    
+    if (all(city_filter != "All cities")){
+      dft <- dft %>%
+        filter(city == city_filter[1], state == city_filter[2])
+    }  
+    dft <- dft %>% arrange(state, city, Year) %>%
+      select(title, Year, description, streetaddress, city, state, amenityfeatures)
+    
+    dft
+  })
     
 }
--- ui.R
+++ ui.R
@@ -14,11 +14,14 @@ fluidRow(
     selectInput("map_amenity_feature", "Choose an establishment feature:", c("Show All", "(PE) - Pretty Elegant", "Miami", "Nashville"), selected = "Show All", multiple = TRUE, selectize = TRUE, width = NULL, size = NULL),
     includeHTML("authors.html")
   )),
+  
   column(9,
-         leafletOutput("spaces_map"),
+          leafletOutput("spaces_map"),
           sliderInput("map.year", "Year",
                        min = 1965, max = 1980,
-                       value = 1900, sep = "", round = TRUE, step = 1, width = "100%")))
-          
+                       value = 1900, sep = "", round = TRUE, step = 1, width = "100%")),
+          dataTableOutput("spaces.table", width="100%", height="auto")
+  )
+
 )
 
