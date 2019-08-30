281dd574c28cda1c599eb19e131eb2416fb4e926
Amanda Regan <regan008@gmail.com>
2019-08-30 15:56:56 -0500

    Make file paths case sensitive for deployment

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-data <- readRDS("NGSdata/data.rds")
+data <- readRDS("NGSData/Data.rds")
 #year_range <- range(bib$year)
 cities <- sort(unique(paste(data$city, data$state, sep = ", ")))
 cities <- c("All cities", cities)
