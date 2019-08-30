38fe89f46fb86782f238a98f932ee8602248093d
Amanda Regan <regan008@gmail.com>
2019-08-30 15:04:28 -0500

    make data filter a reactive function on its own. Must be called now as data.selected()

--- server.R
+++ server.R
@@ -5,47 +5,45 @@ library("dplyr")
 
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
+    data.selected <- reactive({
       
+      city_filter <- strsplit(input$map.city, ", ")[[1]]
+      
+      map.data <- data %>% filter(Year == input$map.year)
+      
+      if (all(city_filter != "All cities")) {
+          map.data <- map.data %>% 
+            filter(city == city_filter[1], state == city_filter[2])
+      }
+      
+      map.data  
+    
+    })
     
    #current.year.data <- data %>% filter(Year == input$map_year)
     
   output$spaces_map <- renderLeaflet({
-    
-    city_filter <- strsplit(input$map.city, ", ")[[1]]
-    
-    data <- data %>% filter(Year == input$map.year)
-    
-    if (all(city_filter != "All cities")) {
-      data <- data %>% 
-        filter(city == city_filter[1], state == city_filter[2])
-    }
       
-      leaflet(data) %>% addTiles() %>%
+    leaflet(data.selected()) %>% addTiles() %>%
         addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
-      
+    
     })
   
   data.for.table <- reactive({
-   
+    
+  
   })
   
   
   output$spaces.table <- renderDataTable({
-    city_filter <- strsplit(input$map.city, ", ")[[1]]
     
-    dft <- data %>%
-      filter(Year == input$map.year)
-    
-    if (all(city_filter != "All cities")){
-      dft <- dft %>%
-        filter(city == city_filter[1], state == city_filter[2])
-    }  
-    dft <- dft %>% arrange(state, city, Year) %>%
+    dft <- data.selected() %>% arrange(state, city, Year) %>%
       select(title, Year, description, streetaddress, city, state, amenityfeatures)
     
-    dft
+   
+    
   })
     
 }
